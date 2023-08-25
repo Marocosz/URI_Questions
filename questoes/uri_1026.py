f12146b88f5c67462d7890f36781d968652826c9
Marcos Rodrigues <ditanixplayer@gmail.com>
2023-08-25 16:29:11 -0300

    Exercicio 2016 finalizado

--- questoes/uri_1026.py
+++ questoes/uri_1026.py
@@ -1,32 +1,11 @@
-n1 = int(input("digite o primeiro número que deseja: "))
-n2 = int(input("digite o segundo número que deseja: "))
+def Main(a, b):
+    print(a ^ b)
 
 
-def transformar_bin(x):
-    global resto
-    lista = []
-
-    while x >= 2:
-        resto = x % 2
-        x = x // 2
-        lista.append(resto)
-
-    if resto <= 1:
-        lista.append(1)
-
-    while len(lista) < 32:
-        if (len(lista) % 8) == 0:
-            lista.append(" ")
-        lista.append(0)
-
-    lista.reverse()
-
-    return lista
-
-
-num1 = transformar_bin(n1)
-num2 = transformar_bin(n2)
-
-print(num1)
-print(num2)
-
+if __name__ == '__main__':
+    while True:
+        try:
+            a, b = map(int, input().split())
+        except EOFError:
+            break
+        Main(a, b)
