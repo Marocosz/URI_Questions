0785b921e95e1688d55393b348ebbbbbb04c3120
Marcos Rodrigues <ditanixplayer@gmail.com>
2023-09-04 12:58:16 -0300

    testezinho de macro

---
+++ Questões_curso_udemy/teste.py
@@ -0,0 +1,14 @@
+import keyboard as kb
+from time import sleep
+
+while True:
+    ativar = kb.is_pressed('ctrl+space')
+    if
+
+    while ativar:
+        a = kb.is_pressed('q')
+
+        if a is True:
+            kb.send('q')
+            print('a')
+            sleep(0.02)
